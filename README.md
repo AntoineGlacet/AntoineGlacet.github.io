2bec8e873d8e88ae8095850e2e969d4db65a91ef
AntoineGlacet <AntoineGlacet@users.noreply.github.com>
2023-01-07 12:22:18 +0000

    jekyll build from Action 6476b9c900faab9f31525c3eeb0263744a408dbd

---
+++ README.md
@@ -0,0 +1,17 @@
+# Personnal blog
+done with [Minimal Mistakes Jekyll theme](https://mmistakes.github.io/minimal-mistakes/)
+
+** code at [develop branch](https://github.com/AntoineGlacet/AntoineGlacet.github.io/tree/develop) **
+
+source code is at `develop` branch and site build by jekyll is at `master` branch.
+
+### To Do list
+
+- [ ] change to gem theme or gem dependencies
+- [ ] comments
+- [ ] email list
+- [ ] 404
+- [ ] more posts (home server media + smart home + keyboard)
+- [ ] dark theme
+- [ ] multi language
+
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Home server with Raspberry Pi",
+        "excerpt":"Set up the perfect Raspberry Pi Ubuntu server Foreword: This guide simply describes how to set up a performant and reliable Ubuntu server on a Raspberry Pi. For about 50-150 USD you will have a fully functioning server that can be used as a media server, smart home hub, ad...","categories": ["home server"],
+        "tags": ["raspberry pi","linux server","ubuntu","media","tutorial"],
+        "url": "/home%20server/rpi-ubuntu-server/",
+        "teaser": "/assets/images/rpi-ubuntu-ssd.png"
+      },{
+        "title": "WSL2+Windows Terminal+VScode",
+        "excerpt":"Windows coding environment Foreword: Windows is king at work and in gamers’ homes, and now it comes with everything you need to code and use linux seamlessly. It is so simple to set up and use that Windows might have actually become linux’s best friend! There is no way around...","categories": ["productivity"],
+        "tags": ["linux","tutorial","coding"],
+        "url": "/productivity/wsl2-terninal-vscode/",
+        "teaser": "/assets/images/wsl-terminal-vscode.png"
+      },{
+        "title": "Mount a drive on Ubuntu",
+        "excerpt":"Mount a drive on Ubuntu Mounting a drive on ubuntu is a rather simple procedure but some important parameters are often forgotten on the tutorials I found online when I first configured my Raspberry Pi as a media server running on docker containers. 1. Find your drive details Many guides...","categories": ["tips"],
+        "tags": ["raspberry pi","linux server","ubuntu","media","tutorial"],
+        "url": "/tips/mount-drive-to-linux/",
+        "teaser": "/assets/images/linux-mount-drive.png"
+      },{
+        "title": "media server with Rasperry Pi and Docker",
+        "excerpt":"Ubuntu Docker media server Foreword: This guide simply describes the media server stack I use on my Raspberry Pi server running on Ubuntu. You can refer directly to my Github repo where all my home server config is stored, including home automation and network tool stacks. A good way to...","categories": ["home server"],
+        "tags": ["raspberry pi","linux server","docker","ubuntu","media","tutorial"],
+        "url": "/home%20server/media-server-rpi-docker/",
+        "teaser": "/assets/images/media-server.png"
+      }]
